f85f271921a4a6c2601b54b9e24532aca557f3f5
Mursaleen <mohdmursaleen9306@gmail.com>
2025-11-01 12:59:44 +0530

    first commit

--- components/header.jsx
+++ components/header.jsx
@@ -3,7 +3,6 @@ import Link from "next/link"
 import { Button } from "./ui/button"
 import { PenBox } from "lucide-react"
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
-import UserMenu from "./user-menu"
 
 const Header = () => {
   return (
@@ -29,7 +28,7 @@ const Header = () => {
               </SignInButton>
             </SignedOut>
             <SignedIn>
-              <UserMenu />
+              <UserButton />
             </SignedIn>
         </div>
     </nav>
