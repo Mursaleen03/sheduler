8c99329995893c9b94a476ec68558e4343223af2
Mursaleen <mohdmursaleen9306@gmail.com>
2025-11-01 11:59:02 +0530

    first commit

---
+++ components/user-menu.jsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs"
+
+const UserMenu = () => {
+  return <UserButton appearance={{
+    elements:{
+        avatarBox: 'w-10 h-10'
+    }
+  }}>
+
+  </UserButton>
+}
+
+export default UserMenu
