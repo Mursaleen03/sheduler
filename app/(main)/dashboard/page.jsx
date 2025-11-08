24acaa6354268ad2a7fb3249b6fceef3828478f7
Mursaleen <mohdmursaleen9306@gmail.com>
2025-11-08 20:21:21 +0530

    username can change

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,7 +1,64 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useUser } from "@clerk/nextjs"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usernameSchema } from "@/app/lib/validators";
+import { useEffect } from "react";
+
+
 const Dashboard = () => {
+  const { user, isLoaded } = useUser();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState:{errors}
+   }= useForm({
+    resolver: zodResolver(usernameSchema),
+  });
+
+  useEffect(() => {
+    setValue("username", user?.username || "");
+  }, [isLoaded])
+  
+  
+  const onSubmit = async (data) => {};
+
   return (
-    <div>
-      Dashboard
+    <div className="space-y-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome, {user?.firstName}!</CardTitle>
+        </CardHeader>
+        {/* Latest Updates */}
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Unique Link</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <div className="flex items-center gap-2">
+                <span>{window?.location.origin}</span>
+                <Input {...register("username")} placeholder="Username" />
+              </div>
+              {errors.username && (
+                <p className="text-red-600 text-sm mt-1">
+                {errors.username.message}
+                </p>
+              )}
+            </div>
+            <Button type="submit">Update Username</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   )
 }
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
   return (
-    <div className="text-4xl font-extrabold w-screen pt-96 grid place-items-center">
+    <div className="text-4xl font-extrabold w-screen pt-80 grid place-items-center">
       <h1>404 - Page Not Found</h1>
     </div>
   )
