38b8c97de6ac38bf871ac3cde76abc8ce97c6b6f
Mursaleen <mohdmursaleen9306@gmail.com>
2025-11-24 13:37:34 +0530

    calender setup

--- actions/events.js
+++ actions/events.js
@@ -1,142 +1,192 @@
-'use server';
+"use server";
 
 import { eventSchema } from "@/app/lib/validators";
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { add, addDays, addMinutes, format, isBefore, parseISO, startOfDay } from "date-fns";
 
 export async function createEvent(data) {
-    const { userId } = await auth();
+  const { userId } = await auth();
 
-    if (!userId) {
-        throw new Error("Unauthorized");
-    }
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
 
-    const validatedData = eventSchema.parse(data);
+  const validatedData = eventSchema.parse(data);
 
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    const user = await db.user.findUnique({
-        where: { clerkUserId: userId },
-    });
-    if (!user) {
-        throw new Error("User not found")
-    }
-
-    const event = await db.event.create({
-        data: {
-            ...validatedData,
-            userId: user.id,
-
-        },
-    });
-    return event;
+  const event = await db.event.create({
+    data: {
+      ...validatedData,
+      userId: user.id,
+    },
+  });
+  return event;
 }
 
 export async function getUserEvents() {
-    const { userId } = await auth();
+  const { userId } = await auth();
 
-    if (!userId) {
-        throw new Error("Unauthorized");
-    }
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
 
-    const user = await db.user.findUnique({
-        where: { clerkUserId: userId },
-    });
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
 
-    if (!user) {
-        throw new Error("User not found")
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    const events = await db.event.findMany({
-        where: { userId: user.id },
-        orderBy: { createdAt: "desc" },
-        include: {
-            _count: {
-                select: { bookings: true },
-            }
-        }
-    });
-    return { events, username: user.username };
+  const events = await db.event.findMany({
+    where: { userId: user.id },
+    orderBy: { createdAt: "desc" },
+    include: {
+      _count: {
+        select: { bookings: true },
+      },
+    },
+  });
+  return { events, username: user.username };
 }
 
 export async function deleteEvent(eventId) {
-    const { userId } = await auth();
+  const { userId } = await auth();
 
-    if (!userId) {
-        throw new Error("Unauthorized");
-    }
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
 
-    const user = await db.user.findUnique({
-        where: { clerkUserId: userId },
-    });
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
 
-    if (!user) {
-        throw new Error("User not found")
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    const event = await db.event.findUnique({
-        where: { id: eventId }
-    });
+  const event = await db.event.findUnique({
+    where: { id: eventId },
+  });
 
-    if (!event || event.userId !== user.id) {
-        throw new Error("Event not found or unauthorized")
-    }
+  if (!event || event.userId !== user.id) {
+    throw new Error("Event not found or unauthorized");
+  }
 
-    await db.event.delete({
-        where: { id: eventId }
-    })
+  await db.event.delete({
+    where: { id: eventId },
+  });
 
-    return { success: true };
+  return { success: true };
 }
 
 export async function getEventDetails(username, eventId) {
-    const event = await db.event.findFirst({
-        where: {
-            id: eventId,
-            user: {
-                username: username,
-            },
+  const event = await db.event.findFirst({
+    where: {
+      id: eventId,
+      user: {
+        username: username,
+      },
+    },
+    include: {
+      user: {
+        select: {
+          name: true,
+          email: true,
+          username: true,
+          imageUrl: true,
         },
-        include: {
-            user: {
-                select: {
-                    name: true,
-                    email: true,
-                    username: true,
-                    imageUrl: true,
-                },
-            },
-        },
-    });
-    return event;
+      },
+    },
+  });
+  return event;
 }
 
 export async function getEventAvailability(eventId, date) {
-    const event = await db.event.findUnique({
-        where: {
-            id: eventId,
-        },
+  const event = await db.event.findUnique({
+    where: {
+      id: eventId,
+    },
+    include: {
+      user: {
         include: {
-            user: {
-                include: {
-                    availability: {
-                        select: {
-                            days: true,
-                            timeGap: true,
-                        }
-                    },
-                    bookings: {
-                        select: {
-                            startTime: true,
-                            endTime: true,
-                        }
-                    }
-                }
+          availability: {
+            select: {
+              days: true,
+              timeGap: true,
             },
+          },
+          bookings: {
+            select: {
+              startTime: true,
+              endTime: true,
+            },
+          },
         },
-    });
-    if(!event && event.user.availability) {
-        return [];
-    }
+      },
+    },
+  });
+  if (!event && event.user.availability) {
+    return [];
+  }
 
-    const { availability, bookings } = event.user;
+  const { availability, bookings } = event.user;
+
+  const startDate = startOfDay(new Date());
+  const endDate = addDays(startDate, 30);
+
+  const availableDates = [];
+
+  for (let date = startDate; date <= endDate; date = addDays(date, 1)) {
+    const dayOfWeek = format(date, "EEEE").toUpperCase();
+    const dayAvailability = availability.days.find((d) => d.day === dayOfWeek);
+    if (dayAvailability) {
+        const dateStr = format(date, "yyyy-MM-dd");
+
+        const slots = generateAvailableTimeSlots(
+            dayAvailability.startTime,
+            dayAvailability.endTime,
+            event.duration,
+            bookings,
+            dateStr,
+            availability.timeGap,
+        );
+
+        availableDates.push({
+            date: dateStr,
+            slots,
+        })
+    }
+  }
+  return availableDates;
+}
+
+function generateAvailableTimeSlots(
+    startTime,
+    endTime,
+    duration, 
+    bookings, 
+    dateStr, 
+    timeGap = 0
+) {
+    const slots = [];
+
+    let currentTime = parseISO(`${dateStr}T${startTime.toISOString().slice(11,16)}`);
+    const slotEndTime = parseISO(`${dateStr}T${endTime}`);
+
+    const now = new Date();
+
+    if(format(now, "yyyy-MM-dd") === dateStr) {
+        currentTime = isBefore(currentTime, now) ? addMinutes(now, timeGap) : currentTime;
+    }
+    while( currentTime < slotEndTime) {
+        const slotEnd = new Date(currentTime.getTime() + duration * 60000);
+    }
 }
